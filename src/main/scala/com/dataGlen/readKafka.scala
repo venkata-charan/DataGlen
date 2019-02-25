73e1c86f9b032f398d22958837f2c7f52db97463
venkata-charan <charanraj.lv@gmail.com>
2019-02-25 11:08:59 +0530

    Initial commit

---
+++ src/main/scala/com/dataGlen/readKafka.scala
@@ -0,0 +1,29 @@
+package com.dataGlen
+
+import org.apache.spark.sql.SparkSession
+
+object  readKafka extends  App{
+
+  val spark = SparkSession
+              .builder()
+              .appName("ReadKafka using Spark session")
+              .getOrCreate()
+
+  val df = spark
+  .readStream
+  .format("kafka")
+  .option("kafka.bootstrap.servers", "ip-172-31-38-146.ec2.internal:6667")
+  .option("subscribe", "walmart_topic")
+  .option("startingOffsets", "earliest")
+  .load() //test
+
+  val df1 = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+  val query = df1.writeStream
+    .outputMode("complete")
+    .format("console")
+    .start()
+
+  query.awaitTermination()
+
+}
