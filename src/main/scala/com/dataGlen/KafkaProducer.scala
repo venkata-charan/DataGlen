6f2f0e2301bdc52f86690f14b407aeb11b4c5882
venkata-charan <charanraj.lv@gmail.com>
2019-02-26 11:44:50 +0530

    Final version

--- src/main/scala/com/dataGlen/KafkaProducer.scala
+++ src/main/scala/com/dataGlen/KafkaProducer.scala
@@ -11,32 +11,38 @@ import scala.util.Random
 object KafkaProducer extends App {
 
 
+  if (args.length < 2) {
+    System.err.println("Usage: KafkaProducer input_topic_name " +
+      " kafka.bootstrap.servers")
+    System.exit(1)
+  }
+
+  //read topic name and broker ip
   val topic = args(0)
   val brokerip = args(1)
   val rnd = new Random()
 
+  //Set the properties
   val props: Properties = new Properties()
   props.put("bootstrap.servers", brokerip)
-  //props.put("transactional.id", "my-transactional-id")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
 
+  //create Kafka producer
   val myProducer = new KafkaProducer[String,Array[Byte]](props)
 
-  //myProducer.initTransactions()
-
   try {
-    //myProducer.beginTransaction()
+
     while(true)
      {
-       println("trying to send messages")
+       //Send records at frequency 5 seconds
        val in_key = "key"+ rnd.nextInt(5)
        val in_value = rnd.nextInt(1000)
        val in_time = LocalDateTime.now().toString
        val msg = "{\"TIMESTAMP\": \"" + in_time + "\", \"val\": "+ in_value+", \"key\": \""+in_key+"\"}"
        val data = new ProducerRecord[String, Array[Byte]](topic, brokerip , msg.getBytes)
        myProducer.send(data)
-      // myProducer.commitTransaction()
+       println("Message sent to " + topic)
        Thread.sleep(5000)
     }
 
