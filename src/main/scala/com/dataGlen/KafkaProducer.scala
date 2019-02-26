84adf878aff8dbed79f2d9af2671d1aad605cf5f
venkata-charan <charanraj.lv@gmail.com>
2019-02-26 10:15:01 +0530

    Simple scala producer

--- src/main/scala/com/dataGlen/KafkaProducer.scala
+++ src/main/scala/com/dataGlen/KafkaProducer.scala
@@ -1,57 +1,61 @@
 package com.dataGlen
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 import java.time.LocalDateTime
-import org.apache.spark.sql.streaming.Trigger
-import scala.concurrent.duration._
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.KafkaException
+import org.apache.kafka.common.errors.{AuthorizationException, OutOfOrderSequenceException, ProducerFencedException}
 import scala.util.Random
 
 object KafkaProducer extends App {
 
 
-  if (args.length < 2) {
-    System.err.println("Usage: ReadKafka topic_name " +
-      "kafka.bootstrap.servers ")
-    System.exit(1)
-  }
-
-  val inp_topic = args(0)
-  val bootstrapIp = args(1)
-
-
-  //Build spark session for streaming
-  val spark = SparkSession
-    .builder()
-    .appName("WriteKafka using Spark session")
-    .getOrCreate()
-
-  //Set the log level
-  spark.sparkContext.setLogLevel("WARN")
-
-  //parse Data key String , (key ,value , timestamp as JSON)
+  val topic = args(0)
+  val brokerip = args(1)
   val rnd = new Random()
 
-  val in_key = "key"+ rnd.nextInt(5)
-  val in_value = rnd.nextInt(1000)
-  val in_time = LocalDateTime.now().toString
-  case class input_data(TIMESTAMP:String,value:Int,key:String)
+  val props: Properties = new Properties()
+  props.put("bootstrap.servers", brokerip)
+  props.put("transactional.id", "my-transactional-id")
+  props.put("key.serializer.class", "kafka.serializer.StringEncoder")
+  props.put("value.serializer.class", "kafka.serializer.StringEncoder")
 
-  val in_df = spark.createDataFrame(Seq(input_data(in_time,in_value,in_key)))
+  val myProducer = new KafkaProducer[String, String](props)
 
-  // prepare key and value for kafka and write it into Kafka
-  val write_kafka = in_df
-    .select(col("key").as("key")
-      ,to_json(struct(in_df.columns.head,in_df.columns.tail:_*)).
-        cast("String").as("value"))
-    .writeStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers",bootstrapIp)
-    .option("topic", inp_topic)
-    .outputMode("append")
-    .trigger(Trigger.ProcessingTime(30.seconds))
-    .start()
+  myProducer.initTransactions()
 
-  write_kafka.awaitTermination()
+  try {
+    myProducer.beginTransaction()
+    while(true)
+     {
+       val in_key = "key"+ rnd.nextInt(5)
+       val in_value = rnd.nextInt(1000)
+       val in_time = LocalDateTime.now().toString
+       val msg = "{\"TIMESTAMP\": \"" + in_time + "\", \"val\": "+ in_value+", \"key\": \""+in_key+"\"}"
+       val data = new ProducerRecord[String, String](topic, brokerip , msg)
+       myProducer.send(data)
+       myProducer.commitTransaction()
+       Thread.sleep(5000)
+    }
+
+  } catch {
+    case e: ProducerFencedException =>
+      println("Not able to send data " + e)
+      myProducer.close()
+
+    case e: OutOfOrderSequenceException =>
+      println("Not able to send data " + e)
+      myProducer.close()
+
+    case e: AuthorizationException =>
+      println("Not able to send data " + e)
+      myProducer.close()
+
+    case e: KafkaException =>
+      println("Exception " + e)
+      myProducer.abortTransaction()
+
+  }
 
 }
